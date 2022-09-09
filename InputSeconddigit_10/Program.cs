370bcdc9e198d0a199443e38d522f91d02790e86
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-09-09 14:12:36 +0300

    input_second_digital

---
+++ InputSeconddigit_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (Math.Abs(number) < 100 || Math.Abs(number) > 1000)
+{
+   Console.WriteLine($"Число {number} не является трёхзначным. Введите число соответствующее условиям задачи");
+}
+else
+{
+   int secondDigit = number / 10 % 10;
+   System.Console.WriteLine($"В введённом числе {number} вторая цифра имеет значение {Math.Abs (secondDigit)}");
+}
