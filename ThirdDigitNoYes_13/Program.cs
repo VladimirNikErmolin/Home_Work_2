6ee8e70ec7d5169842549cc229158fbcfaa61d0b
Vladimir Ermolin <w.ermolin@yahoo.com>
2022-09-09 17:49:36 +0300

    Third_Digit_No_Yes

---
+++ ThirdDigitNoYes_13/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (Math.Abs(number) < 100)
+{
+    Console.WriteLine($"В введеном числе {number} третьей цифры нет");
+}
+else if (number > 0)
+{
+    string thirdDigit = number.ToString();
+    Console.WriteLine($"Третья цифра введённого числа {number} имеет значение {thirdDigit[2]}");
+}
+else
+{
+    string thirdDigit = number.ToString();
+    Console.WriteLine($"Третья цифра введённого числа {number} имеет значение {thirdDigit[3]}");
+}
